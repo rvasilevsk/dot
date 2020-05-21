e50c3564dae7b2653502f67a09fcb10968fc96a4
rvasilevsk <roman.vasilevsk@gmail.com>
2020-05-21 18:20:07 +0300

    tilix vte fix

---
+++ rc.gui/tilix.linux.sh
@@ -0,0 +1,9 @@
+# [Tilix: VTE Configuration](https://gnunn1.github.io/tilix-web/manual/vteconfig/)
+
+# 1. Source vte.sh in bashrc
+# once: ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+# if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+#     source /etc/profile.d/vte.sh
+# fi
+
+# 2. OR use a login shell
