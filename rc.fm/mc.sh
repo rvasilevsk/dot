5e3cccc1ef6549776268f05ded784e6e75ce6390
rvasilevsk <roman.vasilevsk@gmail.com>
2021-04-15 10:41:21 +0300

    mc LANGUAGE en_US.UTF-8

--- rc.fm/mc.sh
+++ rc.fm/mc.sh
@@ -4,11 +4,11 @@
 # mac
 if [ -f /usr/local/Cellar/midnight-commander/4.8.22_2/libexec/mc/mc-wrapper.sh ]
 then
-	alias mc='SHELL=/bin/bash . /usr/local/Cellar/midnight-commander/4.8.22_2/libexec/mc/mc-wrapper.sh' # maconly
+	alias mc='SHELL=/bin/bash source /usr/local/Cellar/midnight-commander/4.8.22_2/libexec/mc/mc-wrapper.sh' # maconly
 fi
 
 # ubuntu
 if [ -f /usr/lib/mc/mc-wrapper.sh ]
 then
-	alias mc='SHELL=/bin/bash . /usr/lib/mc/mc-wrapper.sh'
+	alias mc='SHELL=/bin/bash LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8 source /usr/lib/mc/mc-wrapper.sh'
 fi
