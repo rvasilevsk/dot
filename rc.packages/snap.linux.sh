522bb024da3e589eac8468ceaa519d9246def956
rvasilevsk <roman.vasilevsk@gmail.com>
2022-03-28 14:49:57 +0300

    snap

---
+++ rc.packages/snap.linux.sh
@@ -0,0 +1,21 @@
+snh() {
+    local msg='
+snh - my snap help
+alias snncdu="ncdu /var/lib/snapd/snaps"
+alias snl="snap list"
+alias snla="snap list --all"
+alias sni="snap install"
+alias snu="snap remove"
+alias snch="snap changes"
+alias snup="snap refresh"
+'
+    echo "$msg"
+}
+
+alias snncdu="ncdu /var/lib/snapd/snaps"
+alias snl="snap list"
+alias snla="snap list --all"
+alias sni="snap install"
+alias snu="snap remove"
+alias snch="snap changes"
+alias snup="snap refresh"
