cccc64a1d39ee824670099683d3ce171f2d92326
rvasilevsk <roman.vasilevsk@gmail.com>
2022-03-28 21:31:25 +0300

    snap

--- rc.packages/snap.linux.sh
+++ rc.packages/snap.linux.sh
@@ -8,7 +8,10 @@ alias sni="snap install"
 alias snu="snap remove"
 alias snch="snap changes"
 alias snup="snap refresh"
-'
+snui() { remove/install }
+
+sudo snap set system refresh.retain=2'
+
     echo "$msg"
 }
 
@@ -19,3 +22,10 @@ alias sni="snap install"
 alias snu="snap remove"
 alias snch="snap changes"
 alias snup="snap refresh"
+
+snui() {
+    echo sudo snap remove "$1"
+    sudo snap remove "$1"
+    echo sudo snap install "$1"
+    sudo snap install "$1"
+}
